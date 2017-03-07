5597355a7bb1df54470866e5a2d3d2d1b6d8f6ad
Dawid Dąbkowski <dav.dabkowski@gmail.com>
2017-03-07 08:47:11 +0100

    Praca domowa 1
    Poprawka: poprzednio wysylalem zla, stara wersje projektu

--- MIMUW_2017/PracaDomowa/PD1/Dawid_Dabkowski_Lab1.R
+++ MIMUW_2017/PracaDomowa/PD1/Dawid_Dabkowski_Lab1.R
@@ -6,15 +6,15 @@ library("lattice")
 heights <- heights
 genomes <- genomes
 
-# MODEL 1 (wzrost ¿ony na podstawie wzrostu mê¿a)
+# MODEL 1 (wzrost zony na podstawie wzrostu meza)
 model1 <- lm(Wife ~ Husband, data=heights)
 summary(model1)
 # wykres danych
 xyplot(Husband~Wife, data=heights, type=c("p","r"), pch=19)
-# wykresy diagnostyczne (wygladaj¹ w porz¹dku)
+# wykresy diagnostyczne (wygladaja w porzadku)
 plot(model1)
 
-# MODEL 1log (wzorst ¿ony na podstawie wzrostu mê¿a, dane zlogarytmizowane)
+# MODEL 1log (wzorst zony na podstawie wzrostu meza, dane zlogarytmizowane)
 model1log <- lm(log(Wife) ~ log(Husband), data=heights)
 summary(model1log)
 # wykres danych (prawie nie zmienil sie w skali logarytmicznej)
@@ -46,12 +46,46 @@ rstandard <- lapply(modele,rstandard)
 rstudent <- lapply(modele,rstudent)
 cooks.distance <- lapply(modele,cooks.distance)
 
-# for (i in 1:4) {
-#   cat("Model ", i, "\n")
-#   cat("rstandard: ", head(rstandard[[i]]), "...")
-#   cat(head(rstudent[[i]]))
-#   cat(head(cooks.distance[[i]]))  
-# }
+# Testy na ostatnim modelu
 
-#testynazwy <- c(bptest, gqtest, hmctest, dwtest, bgtest, harvtest, raintest, resettest, shapiro.test)
-#testywyniki <- lapply(modele,)
+# Test Breuscha-Pagana jednorodnosci wariancji
+bptest(model2log)
+# p-wartosc duza, powyzej progu 0.05, przyjmujemy wiec, ze wariancje
+# reszt nie zaleza od zmiennych objasniajacych modelu
+
+# Test Goldfielda-Quandta jednorodnosci wariancji
+gqtest(model2log, order.by=genomes$size, point=0.5)
+# Duza p-wartosc, zatem wariancje reszt nie zaleza istotnie od
+# wielkosci size jesli podzielimy na 2 polowy wzg. tej zmiennej
+
+# Test Harrisona-McCabe jednorodnosci wariancji
+hmctest(model2log, order.by=genomes$size)
+# Inny test na wariancje reszt; znow p-wartosc dostatecznie duza
+
+# Test Durbina-Watsona niezaleznosci reszt
+dwtest(model2log, order.by=genomes$size)
+# p-wartosc bardzo mala, wyglada na to ze wystepuje autokorelacja
+# miedzy resztami a resztami posort. ze wzgl. na zmienna size
+
+# Test Breuscha-Godfreya niezaleznosci reszt
+bgtest(model2log, order.by=genomes$size, order=3)
+# Podobnie jak poprzednio wskazuje to na wystepowanie autokorelacji
+# w tym tescie tez dla wyzszych rzedow (tutaj do rzedu 1,2,3)
+
+# Test Harveya-Colliera liniowosci modelu
+harvtest(model2log, order.by=genomes$size)
+# Wyglada na to ze zaleznosc miedzy zm. objasniajaca a
+# zm. objasniana ma faktycznie charakter liniowy
+
+# Test Rainbow liniowosci modelu
+raintest(model2log, order.by=genomes$size)
+# Wyglada na to ze liniowosc jest wrecz taka sama na calej probie
+# jak i na podzbiorach wyznaczonych wzgledem size
+
+# Test Ramseya RESET
+resettest(model2log, type="regressor")
+# Uwzglednienie wyzszych poteg nie daje lepszej liniowosci
+
+# Test normalnosci reszt
+shapiro.test(model2log$residuals)
+# Reszty nie wygladaja na normalne, co jednak niekoniecznie jest problemem
